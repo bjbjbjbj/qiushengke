512cadbba28b1980ae5860cec951ee9bdd2695f6
xieweijie <BJ@192.168.2.102>
2018-03-26 10:39:34 +0800

    qiushengke blade 提交

---
+++ qiushengke/resources/views/pc/layout/nav_content.blade.php
@@ -0,0 +1,19 @@
+<div class="home"><p class="abox"><a href="index.html"><img src="/pc/img/logo_image_n.png"></a></p></div>
+<div class="Column">
+    <a
+            @if($type == 0)
+            class="on"
+            @endif
+            href="/match/foot/schedule/immediate.html">足球</a>
+    <a
+            @if($type == 1)
+            class="on"
+            @endif
+            href="/match/basket/schedule/immediate_t.html">篮球</a>
+    <a
+            @if($type == 2)
+            class="on"
+            @endif
+            href="/anchor/index.html">主播</a>
+    {{--<a href="">手机APP</a>--}}
+</div>
