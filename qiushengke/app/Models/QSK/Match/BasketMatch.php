04db545f4e06c32bdbd7eab04c7ae1027888e1aa
howlee <112477130@qq.com>
2018-03-27 15:57:25 +0800

    获取章鱼/龙珠直播源

--- qiushengke/app/Models/QSK/Match/BasketMatch.php
+++ qiushengke/app/Models/QSK/Match/BasketMatch.php
@@ -9,9 +9,21 @@
 namespace App\Models\QSK\Match;
 
 
+use App\Models\QSK\Anchor\AnchorRoomMatches;
 use Illuminate\Database\Eloquent\Model;
 
 class BasketMatch extends Model
 {
     protected $connection = 'qsk_match';
+
+    /**
+     * 获取预约了该比赛的主播直播间
+     * @return array
+     */
+    public function liveRooms() {
+        $mid = $this->id;
+        $rooms = AnchorRoomMatches::getRooms($mid, AnchorRoomMatches::kSportBasketball);
+        return $rooms;
+    }
+
 }
