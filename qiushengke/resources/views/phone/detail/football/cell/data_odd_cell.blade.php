64ac6514a36da07d12cfb7ac6e0826c0bc3cef02
howlee <112477130@qq.com>
2018-03-30 23:21:01 +0800

    修改css

--- qiushengke/resources/views/phone/detail/football/cell/data_odd_cell.blade.php
+++ qiushengke/resources/views/phone/detail/football/cell/data_odd_cell.blade.php
@@ -25,9 +25,9 @@
                 <p>{{$odd['asia']['down1']}}</p>
             </td>
             <td>
-                <p class="green">{{$odd['asia']['up2']}}</p>
-                <p>{{\App\Http\Controllers\PC\CommonTool::getOddMiddleString($odd['asia']['middle2'])}}</p>
-                <p>{{$odd['asia']['down2']}}</p>
+                <p class="{{\App\Http\Controllers\PC\CommonTool::colorOfWapUpDown($odd['asia']['up1'], $odd['asia']['up2'])}}">{{$odd['asia']['up2']}}</p>
+                <p class="{{\App\Http\Controllers\PC\CommonTool::colorOfWapUpDown($odd['asia']['middle1'], $odd['asia']['middle2'])}}">{{\App\Http\Controllers\PC\CommonTool::getOddMiddleString($odd['asia']['middle2'])}}</p>
+                <p class="{{\App\Http\Controllers\PC\CommonTool::colorOfWapUpDown($odd['asia']['down1'], $odd['asia']['down2'])}}">{{$odd['asia']['down2']}}</p>
             </td>
         </tr>
     @else
@@ -42,9 +42,9 @@
             <td>欧赔</td>
             <td><p>{{$odd['ou']['up1']}}</p><p>{{$odd['ou']['middle1']}}</p><p>{{$odd['ou']['down1']}}</p></td>
             <td>
-                <p>{{$odd['ou']['up2']}}</p>
-                <p>{{$odd['ou']['middle2']}}</p>
-                <p class="blue">{{$odd['ou']['down2']}}</p>
+                <p class="{{\App\Http\Controllers\PC\CommonTool::colorOfWapUpDown($odd['ou']['up1'], $odd['ou']['up2'])}}">{{$odd['ou']['up2']}}</p>
+                <p class="{{\App\Http\Controllers\PC\CommonTool::colorOfWapUpDown($odd['ou']['middle1'], $odd['ou']['middle2'])}}">{{$odd['ou']['middle2']}}</p>
+                <p class="{{\App\Http\Controllers\PC\CommonTool::colorOfWapUpDown($odd['ou']['down1'], $odd['ou']['down2'])}}">{{$odd['ou']['down2']}}</p>
             </td>
         </tr>
     @else
@@ -59,9 +59,9 @@
             <td>大小球</td>
             <td><p>{{$odd['goal']['up1']}}</p><p>{{$odd['goal']['middle1']}}</p><p>{{$odd['goal']['down1']}}</p></td>
             <td>
-                <p>{{$odd['goal']['up2']}}</p>
-                <p>{{$odd['goal']['middle2']}}</p>
-                <p class="blue">{{$odd['goal']['down2']}}</p>
+                <p class="{{\App\Http\Controllers\PC\CommonTool::colorOfWapUpDown($odd['goal']['up1'], $odd['goal']['up2'])}}">{{$odd['goal']['up2']}}</p>
+                <p class="{{\App\Http\Controllers\PC\CommonTool::colorOfWapUpDown($odd['goal']['middle1'], $odd['goal']['middle2'])}}">{{$odd['goal']['middle2']}}</p>
+                <p class="{{\App\Http\Controllers\PC\CommonTool::colorOfWapUpDown($odd['goal']['down1'], $odd['goal']['down2'])}}">{{$odd['goal']['down2']}}</p>
             </td>
         </tr>
     @else
