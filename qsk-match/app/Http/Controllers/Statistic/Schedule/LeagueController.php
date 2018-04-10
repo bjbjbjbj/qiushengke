ad49a0b4897071707676f6971e09e04400f76060
ricky <843372983@qq.com>
2018-04-10 18:06:15 +0800

    match 篮球积分排名……

--- qsk-match/app/Http/Controllers/Statistic/Schedule/LeagueController.php
+++ qsk-match/app/Http/Controllers/Statistic/Schedule/LeagueController.php
@@ -5,7 +5,9 @@ use App\Http\Controllers\Statistic\OddCalculateTool;
 use App\Http\Controllers\Statistic\StatisticFileTool;
 use App\Models\LiaoGouModels\BasketMatch;
 use App\Models\LiaoGouModels\BasketLeague;
+use App\Models\LiaoGouModels\BasketScore;
 use App\Models\LiaoGouModels\BasketSeason;
+use App\Models\LiaoGouModels\BasketStage;
 use App\Models\LiaoGouModels\BasketTeam;
 use App\Models\LiaoGouModels\League;
 use App\Models\LiaoGouModels\Match;
@@ -189,6 +191,9 @@ class LeagueController{
                 $aid = $match->aid;
                 $key = $hid . '_' . $aid;
                 $key2 = $aid . '_' . $hid;
+                if (array_key_exists($key2,$result)) {
+                    $key = $key2;
+                }
                 //先看看是否有之前的比赛是主客互换
                 if (!array_key_exists($key, $result) && !array_key_exists($key2, $result)) {
                     $hname = $match->hname;
@@ -272,7 +277,9 @@ class LeagueController{
                 $join->on('scores.tid', '=', 'team.id');
             })
             ->whereNull('kind')
-            ->addSelect('scores.*')
+            ->addSelect('scores.lid', 'scores.lsid', 'scores.season', 'scores.tid', 'scores.count'
+                ,'scores.score', 'scores.goal', 'scores.fumble', 'scores.win', 'scores.draw'
+                ,'scores.lose', 'scores.status', 'scores.name', 'scores.rank', 'scores.kind')
             ->addSelect('team.name as tname','team.icon as ticon')
             ->orderby('rank','asc')
             ->get();
@@ -397,7 +404,7 @@ class LeagueController{
         $result['league'] = $league;
         //赛季
         $season = BasketSeason::query()
-            ->select("name", "year", "kind")
+            ->select("name", "year", "kind", "total_kind")
             ->where('lid',$lid)
             ->orderby('year','desc')
             ->first();
@@ -411,8 +418,71 @@ class LeagueController{
             //联赛
         $result['schedule'] = $this->_getLeagueSecheduleMatchBK($league->id,$seasonStr,$start,$end);
 //        }
+        if ($league->type == 1) {
+            $zones = BasketScore::from('basket_scores as s')
+                    ->select('s.zone')->where('s.lid', $league->id)->where('s.season', $seasonStr)
+                    ->groupBy('s.zone', 's.lid', 's.season')->get();
+                foreach ($zones as $zone) {
+                    $zoneId = $zone->zone;
+                    //积分榜
+                    $result['score'][$zoneId] = $this->_getLeagueScoreBK($league, $seasonStr, $zoneId);
+                }
+            }
         return $result;
     }
+
+//    //获取篮球赛程,当前到后7天
+//    public function getLeagueScheduleBK($lid, $start = null){
+//        if (is_null($start)){
+//            $start = date('Y-m-d');
+//        }
+//
+//        $end = strtotime($start) + 7*24*3600;
+//        $end = date('Y-m-d',$end);
+//
+//        //赛事
+//        $league = BasketLeague::query()->select("id", "name","type","system","hot", "describe")->find($lid);
+//        if (is_null($league)){
+//            return null;
+//        }
+//        $result['league'] = $league;
+//        //赛季
+//        $season = BasketSeason::query()
+//            ->select("name", "year", "kind", "total_kind")
+//            ->where('lid',$lid)
+//            ->orderby('year','desc')
+//            ->offset(1)
+//            ->first();
+//        if (is_null($season)){
+//            return null;
+//        }
+//        $seasonStr = $season->name;
+//        $result['season'] = $season;
+//        //整理比赛
+//        if ($league->type == 1){
+//            $seasonKinds = explode(",", $season->total_kind);
+//            foreach ($seasonKinds as $kind) {
+//                if ($kind == 2) {
+//                    $result[$kind]['stages'] = $this->_getCupLeagueScheduleBK($league,$seasonStr,$kind);
+//                } else {
+//                    //联赛
+//                    $result[$kind]['schedule'] = $this->_getLeagueSecheduleMatchBK($league->id, $seasonStr, $start, $end);
+//                    $zones = BasketScore::from('basket_scores as s')
+//                        ->select('s.zone')->where('s.lid', $league->id)->where('s.season', $seasonStr)
+//                        ->groupBy('s.zone', 's.lid', 's.season')->get();
+//                    foreach ($zones as $zone) {
+//                        $zoneId = $zone->zone;
+//                        //积分榜
+//                        $result[$kind]['score'][$zoneId] = $this->_getLeagueScoreBK($league, $seasonStr, $zoneId);
+//                    }
+//                }
+//            }
+//        } else {
+//            //杯赛
+//            $result['stages'] = $this->_getCupLeagueScheduleBK($league,$seasonStr);
+//        }
+//        return $result;
+//    }
 
     public function getLeagueScheduleBkByDate(Request $request, $lid) {
         //赛事
@@ -422,7 +492,7 @@ class LeagueController{
         }
         //赛季
         $season = BasketSeason::query()
-            ->select("name", "year", "kind")
+            ->select("name", "year", "kind", "total_kind")
             ->where('lid',$lid)
             ->orderby('year','desc')
             ->first();
@@ -438,8 +508,8 @@ class LeagueController{
         return response()->json($matches);
     }
 
-    private function _getLeagueSecheduleMatchBK($lid,$seasonStr,$start,$end){
-        $matches = BasketMatch::from("basket_matches as m")
+    private function _getLeagueSecheduleMatchBK($lid,$seasonStr,$start,$end,$kind = null){
+        $query = BasketMatch::from("basket_matches as m")
             ->addSelect('m.id as mid', 'm.hid', 'm.aid')
             ->leftjoin('basket_odds as asia', function ($join) {
                 $join->on('m.id', '=', 'asia.mid');
@@ -469,8 +539,13 @@ class LeagueController{
             ->where('lid',$lid)
             ->where('season',$seasonStr)
             ->where('time','>=',$start)
-            ->where('time','<',$end)
-            ->orderby('time','asc')
+            ->where('time','<',$end);
+
+        if (isset($kind) && $kind > 0) {
+            $query->where('kind', $seasonStr);
+        }
+
+        $matches = $query->orderby('time','asc')
             ->get();
         $result = array();
         foreach ($matches as $match){
@@ -488,5 +563,198 @@ class LeagueController{
             $result[] = $match;
         }
         return $result;
+    }
+
+    //获取杯赛赛程
+    private function _getCupLeagueScheduleBK($league,$seasonStr,$kind = 4){
+        $stages = BasketStage::where('lid',$league->id)
+            ->where('season',$seasonStr)
+            ->where('kind',$kind)
+            ->orderby('id','asc')
+            ->get();
+
+        $array = array();
+
+        //获取小组赛积分数据
+        $scores = BasketScore::query()->select('stage')
+            ->where('lid', $league->id)->where('season', $seasonStr)->whereNotNull('stage')
+            ->groupBy('stage', 'lid', 'season')->get();
+        $groupStage = 0;
+        if (isset($scores) && isset($scores[0])) {
+            $groupStage = $scores[0]->stage;
+        }
+
+        foreach ($stages as $stage){
+            $stage->makeHidden('win_id');
+
+            $isGroup = $stage->id == $groupStage;
+            //看看是否主客互换(16强之类)
+            $tmp = $this->_getCupLeagueStageMatchesBK($league->id,$seasonStr,$stage->id,$isGroup);
+            if ($isGroup){
+                $stage['groupMatch'] = $tmp['groupMatch'];
+            }
+            else if ($tmp['combo'] == 1 && !$isGroup){
+                $stage['combo'] = $tmp['comboMatch'];
+            }
+            else{
+                $stage['matches'] = $tmp['matches'];
+            }
+            $array[] = $stage;
+        }
+
+        return $array;
+    }
+
+    //联赛积分
+    private function _getLeagueScoreBK($league,$seasonStr,$zone = 0){
+        $scores = BasketScore::from('basket_scores as s')
+            ->where('s.lid',$league->id)
+            ->where('s.season',$seasonStr)
+            ->where('s.zone',$zone)
+            ->leftjoin('basket_teams as team', function ($join) {
+                $join->on('s.tid', '=', 'team.id');
+            })
+            ->addSelect('s.lid', 's.season', 's.tid', 's.count', 's.win'
+                ,'s.lose', 's.goal', 's.fumble', 's.win_diff', 's.home_bat_w'
+                ,'s.home_bat_l', 's.away_bat_w', 's.away_bat_l', 's.league_bat_w', 's.league_bat_l'
+                ,'s.zone_bat_w', 's.zone_bat_l', 's.ten_bat_w', 's.ten_bat_l', 's.win_status', 's.rank')
+            ->addSelect('team.name_china as tname','team.icon as ticon')
+            ->orderby('rank','asc')
+            ->get();
+
+        return $scores;
+    }
+
+    //获取比赛赛程对弈比赛列表
+    private function _getCupLeagueStageMatchesBK($lid,$season,$stage,$isGroup){
+        $matches = BasketMatch::from("basket_matches as m")
+            ->addSelect('m.id as mid', 'm.hid', 'm.aid')
+            ->leftjoin('basket_odds as asia', function ($join) {
+                $join->on('m.id', '=', 'asia.mid');
+                $join->where('asia.type', '=', Odd::k_odd_type_asian);
+                $join->where('asia.cid', '=', Odd::default_calculate_cid);
+            })->leftjoin('basket_odds as ou', function ($join) {
+                $join->on('m.id', '=', 'ou.mid');
+                $join->where('ou.type', '=', Odd::k_odd_type_europe);
+                $join->where('ou.cid', '=', Odd::default_calculate_cid);
+            })->leftjoin('basket_odds as goal', function ($join) {
+                $join->on('m.id', '=', 'goal.mid');
+                $join->where('goal.type', '=', Odd::k_odd_type_ou);
+                $join->where('goal.cid', '=', Odd::default_calculate_cid);
+            })
+            ->addSelect('asia.up1 as asiaup1', 'asia.middle1 as asiamiddle1', 'asia.down1 as asiadown1',
+                'asia.up2 as asiaup2', 'asia.middle2 as asiamiddle2', 'asia.down2 as asiadown2',
+                'ou.up1 as ouup1', 'ou.middle1 as oumiddle1', 'ou.down1 as oudown1',
+                'ou.up2 as ouup2', 'ou.middle2 as oumiddle2', 'ou.down2 as oudown2',
+                'goal.up1 as goalup1', 'goal.middle1 as goalmiddle1', 'goal.down1 as goaldown1',
+                'goal.up2 as goalup2', 'goal.middle2 as goalmiddle2', 'goal.down2 as goaldown2')
+            ->leftJoin("basket_teams as home", "m.hid", "home.id")
+            ->leftJoin("basket_teams as away", "m.aid", "away.id")
+            ->addSelect('m.group','m.lid', 'm.status','m.betting_num',
+                'm.time', 'm.timehalf', 'm.hname','m.aname', 'm.hscore','m.ascore')
+            ->addSelect("home.icon as hicon", "away.icon as aicon")
+            ->where('lid',$lid)
+            ->where('season',$season)
+            ->where('stage',$stage)
+            ->orderby('time','asc')
+            ->get();
+
+        $result = array();
+
+        if (!$isGroup) {
+            //看看是否主客互换(16强之类)
+            foreach ($matches as $match) {
+                $match->makeHidden('timehalf');
+                $match = OddCalculateTool::formatOddData($match);
+                if (isset($match->betting_num)) {
+                    $match->betting_num = str_replace(" ", "", $match->betting_num);
+                }
+                $match->hicon = BasketTeam::getIcon($match->hicon);
+                $match->aicon = BasketTeam::getIcon($match->aicon);
+
+                $match->sport = MatchLive::kSportBasketball;
+                $match->time = strtotime($match->time);
+
+                $hid = $match->hid;
+                $aid = $match->aid;
+                $key = $hid . '_' . $aid;
+                $key2 = $aid . '_' . $hid;
+                $tempKey = $key;
+                if (array_key_exists($key2, $result)) {
+                    $tempKey = $key2;
+                }
+                //先看看是否有之前的比赛是主客互换
+                $isHome = explode("_", $tempKey)[0] == $hid;
+                if (!array_key_exists($tempKey, $result)) {
+                    $hname = $match->hname;
+                    $aname = $match->aname;
+                    $result[$tempKey] = array();
+                    $result[$tempKey]['hname'] = $hname;
+                    $result[$tempKey]['aname'] = $aname;
+                    $result[$tempKey]['hid'] = $hid;
+                    $result[$tempKey]['aid'] = $aid;
+                    $result[$tempKey]['matches'] = array();
+                    $result[$tempKey]['matches'][] = $match;
+                    if ($match->status == -1){
+                        $isHomeWin = $isHome ? $match->hscore > $match->ascore : $match->hscore < $match->ascore;
+                        $result[$tempKey]['hscore'] = $isHomeWin ? 1 : 0;
+                        $result[$tempKey]['ascore'] = $isHomeWin ? 0 : 1;
+                    }
+                } else {
+                    $result[$tempKey]['matches'][] = $match;
+                    if ($match->status == -1){
+                        $isHomeWin = $isHome ? $match->hscore > $match->ascore : $match->hscore < $match->ascore;
+                        $result[$tempKey]['hscore'] += $isHomeWin ? 1 : 0;
+                        $result[$tempKey]['ascore'] += $isHomeWin ? 0 : 1;
+                    }
+                }
+            }
+            return array('combo' => 1, 'comboMatch' => $result, 'matches' => $matches);
+        }
+        else{
+            //小组赛
+            foreach ($matches as $match){
+                $match->makeHidden('timehalf');
+                $match = OddCalculateTool::formatOddData($match);
+                if (isset($match->betting_num)) {
+                    $match->betting_num = str_replace(" ", "", $match->betting_num);
+                }
+                $match->hicon = BasketTeam::getIcon($match->hicon);
+                $match->aicon = BasketTeam::getIcon($match->aicon);
+
+                $match->sport = MatchLive::kSportBasketball;
+                $match->time = strtotime($match->time);
+
+                $group = $match['group'];
+                if (!array_key_exists($group,$result)){
+                    $result[$group] = array();
+                    $result[$group]['matches'] = array();
+                }
+                $result[$group]['matches'][] = $match;
+            }
+            ksort($result);
+
+            //积分
+            foreach ($result as $key=>$item){
+                $scores = BasketScore::from("basket_scores as s")->where('lid',$lid)
+                    ->leftjoin('basket_teams as team', function ($join) {
+                        $join->on('s.tid', '=', 'team.id');
+                    })
+                    ->addSelect('s.lid', 's.season', 's.tid', 's.count', 's.win'
+                        ,'s.lose', 's.goal', 's.fumble', 's.win_diff', 's.home_bat_w'
+                        ,'s.home_bat_l', 's.away_bat_w', 's.away_bat_l', 's.league_bat_w', 's.league_bat_l'
+                        ,'s.zone_bat_w', 's.zone_bat_l', 's.ten_bat_w', 's.ten_bat_l', 's.win_status', 's.rank')
+                    ->addSelect('team.name_china as tname','team.icon as ticon')
+                    ->where('season',$season)
+                    ->where('group',$key)
+                    ->where('stage',$stage)
+                    ->orderby('rank','asc')
+                    ->get();
+
+                $result[$key]['scores'] = $scores;
+            }
+
+            return array('combo' => 0, 'groupMatch' => $result);
+        }
     }
 }
